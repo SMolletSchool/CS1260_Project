655a9871d23c4f702062f65bdb64c748c5388b3f
SMolletSchool <shmollet@cpp.edu>
2021-10-18 18:16:51 -0700

    uploaded main_script as .py, and added proposed changes to csv reader

--- csv_reading_ahhad.py
+++ csv_reading_ahhad.py
@@ -3,12 +3,19 @@
 
 # In[6]:
 
-
+#REPLACE
 fileHandle = open("C:\\Users\\ayesh\\Downloads\\test.csv","r")#to copy file path, hold shift and then right click file. open(file path, mode, buffering). 
 #Cannot use string as file path, either put 'r' before file path, "\\" to read every escape character, or replace every "\" with "/"
 for line in fileHandle:
     print(line)
 fileHandle.close()#always close after opening
+#WITH
+#def read_file(user_input):
+#    file = open(user_input)
+#    file_contents = []
+#    for line in file:
+#        file_contents.append(line)
+#    return file_contents
 
 
 # In[ ]:
---
+++ main_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import csv_reading_ahhad
+
+fileInput = input("Enter name of csv file in same directory")
+lines = csv_reading_ahhad.fileHandle(fileInput)
+
