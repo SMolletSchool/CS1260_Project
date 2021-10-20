3be0c7d46592996353c94f00ab35b5d04df2eb0a
kendrickofmars <ahhadmukhtar@gmail.com>
2021-10-20 15:44:35 -0700

    updated csv reader
    changed file reader to check for call from main and take user input for file path

--- csv_reading_ahhad.py
+++ csv_reading_ahhad.py
@@ -1,21 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[13]:
 
-#REPLACE
-fileHandle = open("C:\\Users\\ayesh\\Downloads\\test.csv","r")#to copy file path, hold shift and then right click file. open(file path, mode, buffering). 
+
+#user_input = input(("Input file path: (ex. \"C:\\Users\\insertusernamehere\\Downloads\\test.csv)\"\n"))#to copy file path, hold shift and then right click file. open(file path, mode, buffering). 
 #Cannot use string as file path, either put 'r' before file path, "\\" to read every escape character, or replace every "\" with "/"
-for line in fileHandle:
-    print(line)
-fileHandle.close()#always close after opening
-#WITH
-#def read_file(user_input):
-#    file = open(user_input)
-#    file_contents = []
-#    for line in file:
-#        file_contents.append(line)
-#    return file_contents
+    
+
+def read_file():   
+   # user_input = input(("Input file path: (ex. \"C:\\\\Users\\\\usernamehere\\\\Location\\\\test.csv)\"\n"))#this function takes in user_input as an argument (file path)
+    fileHandle = open(input(("Input file path: (ex. \"C:\\\\Users\\\\usernamehere\\\\Location\\\\test.csv)\"\n")),"r")         
+    file_data = []              #empty list to be written to
+    for line in fileHandle:             
+        file_data.append(line) #writing data of csv to empty list
+    if __name__ == "__main__":   
+        print(file_data)
+    
+    fileHandle.close() 
+    return file_data   
+
+if __name__ == "__main__":
+    read_file()
+
+ #call to read_file passing user_input as argument
+#pathfinder() #call to pathfinder function
+                         
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
